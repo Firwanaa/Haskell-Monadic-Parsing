a107013316d59c2594fa18c2ddd008f4781e4d4b
firwanaa <firwanaa@sheridancollege.ca>
2022-07-01 06:18:23 -0400

    calculator

--- code.hs
+++ code.hs
@@ -223,3 +223,79 @@ eval xs = case (parse expr xs) of
 -- ***Exception: Unused input
 -- > eval "one plus two"
 -- ***Exception: Invalid input
+
+box :: [String]
+box = ["+---------------+",
+       "| |",
+       "+---+---+---+---+",
+       "| q | c | d | = |",
+       "+---+---+---+---+",
+       "| 1 | 2 | 3 | + |",
+       "+---+---+---+---+",
+       "| 4 | 5 | 6 | - |",
+       "+---+---+---+---+",
+       "| 7 | 8 | 9 | * |",
+       "+---+---+---+---+",
+       "| 0 | ( | ) | / |",
+       "+---+---+---+---+"]
+
+-- Buttons
+buttons :: String
+buttons = standard ++ extra
+          where
+            standard = "qcd=123+456-789*0()/"
+            extra = "QCD \ESC\BS\DEL\n"
+
+-- showbox
+showbox :: IO ()
+showbox = sequence_ [writeat (1,y) b | (y,b) <- zip [1..] box]
+
+-- display
+display xs = do writeat (3,2) (replicate 13 ’ ’)
+                writeat (3,2) (reverse (take 13 (reverse xs)))
+
+-- calc
+calc :: String -> IO ()
+calc xs = do display xs
+          c <- getCh
+          if elem c buttons then
+              process c xs
+          else
+              do beep
+                 calc xs
+-- process
+process :: Char -> String -> IO ()
+process c xs | elem c "qQ\ESC" = quit
+             | elem c "dD\BS\DEL" = delete xs
+             | elem c "=\n" = eval xs
+             | elem c "cC" = clear
+             | otherwise = press c xs
+
+-- quit
+quit :: IO ()
+quit = goto (1,14)
+
+-- delete
+delete :: String -> IO ()
+delete [] = calc []
+delete xs = calc (init xs)
+
+-- eval
+eval :: String -> IO ()
+eval xs = case parse expr xs of
+             [(n,[])] -> calc (show n)
+             _ -> do beep
+                     calc xs
+-- clear
+clear :: IO ()
+clear = calc []
+
+-- press
+press :: Char -> String -> IO ()
+press c xs = calc (xs ++ [c])
+
+-- run
+run :: IO ()
+run = do cls
+         showbox
+         clear

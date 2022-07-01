605dad691b9c019d6b8b901125a2f5c9d5431482
firwanaa <firwanaa@sheridancollege.ca>
2022-07-01 06:09:29 -0400

    Arithmetic expressions

--- code.hs
+++ code.hs
@@ -186,3 +186,40 @@ nats = do symbol "["
 -- [([1,2,3],"")]
 -- > parse nats "[1,2,]"
 -- []
+
+expr :: Parser Int
+expr = do t <- term
+          do symbol "+"
+          e <- expr
+          return (t + e)
+          <|> return t
+
+term :: Parser Int
+term = do f <- factor
+          do symbol "*"
+          t <- term
+          return (f * t)
+          <|> return
+
+factor :: Parser Int
+factor = do symbol "("
+            e <- expr
+            symbol ")"
+            return e
+         <|> natural
+
+eval :: String -> Int
+eval xs = case (parse expr xs) of
+             [(n,[])] -> n
+             [(_,out)] -> error ("Unused input " ++ out)
+             [] -> error "Invalid input"
+
+-- examples
+-- > eval "2*3+4"
+-- 10
+-- > eval "2*(3+4)"
+-- 14
+-- > eval "2*3^4"
+-- ***Exception: Unused input
+-- > eval "one plus two"
+-- ***Exception: Invalid input
